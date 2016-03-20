bc4b105a6c07c8ee60e1e2aac4b4ea76cf3a4903
Gary Meehan <garymeehan@outlook.com>
2016-03-20 17:53:58 +0000

    Styling bug fixes

--- app/js/controllers/search-controller.js
+++ app/js/controllers/search-controller.js
@@ -10,7 +10,7 @@ module.exports = function($scope, $http){
     $scope.searchQuery = query;
     $http.get('https://crossorigin.me/https://api.brewerydb.com/v2/search?q='+query+'&withBreweries=Y&type=beer&key=8dde036c69009d31acb2d710b19c6e80&format=json')
     .then(function(response){
-      $scope.results = response.data.data;
+      $scope.results = response.data.data;  
     });
   }
 
