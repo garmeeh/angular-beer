57bafa85828bfc76cd53d794571ce29b5204e52f
Gary Meehan <garymeehan@outlook.com>
2016-03-20 11:44:36 +0000

    Added back crossorigin.me

--- app/js/services/beer-factory.js
+++ app/js/services/beer-factory.js
@@ -9,10 +9,9 @@ module.exports = function($http, $q){
     return {
       getBeer: function(bustCache){
         var defer = $q.defer();
-        // Use chrome flags for now: open -a Google\ Chrome --args --disable-web-security --user-data-dir
-        // Add back in once crossorigin.me is back up
-        // http://crossorigin.me/http://api.brewerydb.com/v2/beer/random?withBreweries=Y&hasLabels=Y&key=8dde036c69009d31acb2d710b19c6e80&format=json
-        $http.get('http://api.brewerydb.com/v2/beer/random?withBreweries=Y&hasLabels=Y&key=8dde036c69009d31acb2d710b19c6e80&format=json?' + bustCache)
+        // Remove crossorigin.me prefix and use chrome flags if crossorigin.me goes down.
+        // -> open -a Google\ Chrome --args --disable-web-security --user-data-dir
+        $http.get('http://crossorigin.me/http://api.brewerydb.com/v2/beer/random?withBreweries=Y&hasLabels=Y&key=8dde036c69009d31acb2d710b19c6e80&format=json?' + bustCache)
         .success(function(data) {
           defer.resolve(data);
         })
--- app/js/services/brewery-factory.js
+++ app/js/services/brewery-factory.js
@@ -8,7 +8,7 @@ module.exports = function($http, $q){
     return {
       getBrewery: function(breweryId){
         var defer = $q.defer();
-        $http.get('https://crossorigin.me/https://api.brewerydb.com/v2/brewery/'+ breweryId +'?withBreweries=Y&hasLabels=Y&key=8dde036c69009d31acb2d710b19c6e80&format=json')
+        $http.get('https://crossorigin.me/https://api.brewerydb.com/v2/brewery/'+ breweryId +'?key=8dde036c69009d31acb2d710b19c6e80&format=json')
         .success(function(data) {
           defer.resolve(data);
         })
